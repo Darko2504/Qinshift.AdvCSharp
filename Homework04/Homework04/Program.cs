64c7e465c7cd1bad5b3481498256abd4aa167af8
darko <darkomilanovski45@gmail.com>
2025-05-14 23:20:58 +0200

    Homework04 added

---
+++ Homework04/Homework04/Program.cs
@@ -0,0 +1,114 @@
+﻿using Exercise01;
+
+List<Dog> dogs = new List<Dog>()
+{
+    new Dog("Charlie", "Black", 3, Race.Collie), // 0
+    new Dog("Buddy", "Brown", 1, Race.Doberman),
+    new Dog("Max", "Olive", 1, Race.Plott),
+    new Dog("Archie", "Black", 2, Race.Mutt),
+    new Dog("Oscar", "White", 1, Race.Mudi),
+    new Dog("Toby", "Maroon", 3, Race.Bulldog), // 5
+    new Dog("Ollie", "Silver", 4, Race.Dalmatian),
+    new Dog("Bailey", "White", 4, Race.Pointer),
+    new Dog("Frankie", "Gray", 2, Race.Pug),
+    new Dog("Jack", "Black", 5, Race.Dalmatian),
+    new Dog("Chanel", "Black", 1, Race.Pug), // 10
+    new Dog("Henry", "White", 7, Race.Plott),
+    new Dog("Bo", "Maroon", 1, Race.Boxer),
+    new Dog("Scout", "Olive", 2, Race.Boxer),
+    new Dog("Ellie", "Brown", 6, Race.Doberman),
+    new Dog("Hank", "Silver", 2, Race.Collie), // 15
+    new Dog("Shadow", "Silver", 3, Race.Mudi),
+    new Dog("Diesel", "Brown", 4, Race.Bulldog),
+    new Dog("Abby", "Black", 1, Race.Dalmatian),
+    new Dog("Trixie", "White", 8, Race.Pointer), // 19
+};
+
+List<Person> people = new List<Person>()
+{
+    new Person("Nathanael", "Holt", 20, Job.Choreographer),
+    new Person("Rick", "Chapman", 35, Job.Dentist),
+    new Person("Oswaldo", "Wilson", 19, Job.Developer),
+    new Person("Kody", "Walton", 43, Job.Sculptor),
+    new Person("Andreas", "Weeks", 17, Job.Developer),
+    new Person("Kayla", "Douglas", 28, Job.Developer),
+    new Person("Richie", "Campbell", 19, Job.Waiter),
+    new Person("Soren", "Velasquez", 33, Job.Interpreter),
+    new Person("August", "Rubio", 21, Job.Developer),
+    new Person("Rocky", "Mcgee", 57, Job.Barber),
+    new Person("Emerson", "Rollins", 42, Job.Choreographer),
+    new Person("Everett", "Parks", 39, Job.Sculptor),
+    new Person("Amelia", "Moody", 24, Job.Waiter)
+    { Dogs = new List<Dog>() {dogs[16], dogs[18] } },
+    new Person("Emilie", "Horn", 16, Job.Waiter),
+    new Person("Leroy", "Baker", 44, Job.Interpreter),
+    new Person("Nathen", "Higgins", 60, Job.Archivist)
+    { Dogs = new List<Dog>(){dogs[6], dogs[0] } },
+    new Person("Erin", "Rocha", 37, Job.Developer)
+    { Dogs = new List<Dog>() {dogs[2], dogs[3], dogs[19] } },
+    new Person("Freddy", "Gordon", 26, Job.Sculptor)
+    { Dogs = new List<Dog>() {dogs[4], dogs[5], dogs[10], dogs[12], dogs[13] } },
+    new Person("Valeria", "Reynolds", 26, Job.Dentist),
+    new Person("Cristofer", "Stanley", 28, Job.Dentist)
+    { Dogs = new List<Dog>() {dogs[9], dogs[14], dogs[15] } }
+};
+
+Console.WriteLine("========================= 1 ===========================");
+//1 baranje
+//IEnumerable<Per> firstNameWithRSql = from p in people
+//                                 where p.FirstName.StartsWith("R")
+//                                 orderby p.Age
+//                                 select p.FirstName;
+                                 
+List<string> firstNameWithR = people
+       .OrderByDescending(x => x.Age)
+       .Select(x => x.FirstName)
+       .Where(x => x.StartsWith("R"))
+       .ToList();
+firstNameWithR.PrintSimple();
+
+Console.WriteLine("====================== 2 ======================");
+
+//2 baranje
+List<string> brownDogsNamesOlderThanThree = dogs
+        .Where(x => x.Color == "Brown" && x.Age > 3)
+        .OrderBy(x => x.Age)
+        .Select(x => $"{x.Name} {x.Age}")
+        .ToList();
+brownDogsNamesOlderThanThree.PrintSimple();
+Console.WriteLine("==================== 3 =======================");
+
+//3 baranje
+List<string> personWithMoreThanTwoDogs = people
+        .Where(x => x.Dogs.Count > 2)
+        .OrderByDescending(x => x.FirstName)
+        .Select(x => x.FirstName)
+        .ToList();
+personWithMoreThanTwoDogs.PrintSimple();
+Console.WriteLine("====================== 4 ======================");
+
+//4 baranje
+List<string> allFredysDogsNames = people
+         .Single(x => x.FirstName == "Freddy").Dogs
+         .Where(d => d.Age > 1)
+         .Select(d => d.Name)
+         .ToList();
+allFredysDogsNames.PrintSimple();
+Console.WriteLine("====================== 5 ======================");
+
+//5 baranje
+Dog nathensFirstDog = people
+        .Single(x => x.FirstName == "Nathen").Dogs
+        .First();
+Console.WriteLine(nathensFirstDog.Name);
+Console.WriteLine("======================= 6 =====================");
+
+//6 baranje
+List<string> dogOwners = new() { "Cristofer", "Freddy", "Erin", "Amelia" };
+List<string> dogOwnerNames = people
+    .Where(x => dogOwners.Contains(x.FirstName))
+    .SelectMany(x => x.Dogs)
+    .Where(d => d.Color.ToLower() == "white")
+    .OrderBy(d => d.Name)
+    .Select(d => d.Name).ToList();
+dogOwnerNames.PrintSimple();
---
+++ Homework04/OfficialHomework/Program.cs
@@ -0,0 +1,208 @@
+﻿
+using System.Collections.Generic;
+using OfficialHomework;
+
+//// 1 baranje
+//List<Car> allCarsFromEurope = CarsData.Cars
+//                        .Where(x => x.Origin == "Europe").ToList();
+//allCarsFromEurope.PrintEntities();
+
+//// 2 baranje
+//List<int> uniqueCylinderValues = CarsData.Cars
+//                         .Select(x => x.Cylinders)
+//                         .Distinct()
+//                         .ToList();
+//uniqueCylinderValues.PrintSimple();
+
+////3 baranje
+//List<string> allCarsNamesConverted = CarsData.Cars
+//                          .Select(x => x.Model.ToUpper())
+//                          .ToList();
+//allCarsNamesConverted.PrintSimple();
+
+////4 baranje
+//bool aboveThreeHundredHp = CarsData.Cars
+//                          .Any(x => x.HorsePower > 300);
+//Console.WriteLine(aboveThreeHundredHp);
+
+////5 baranje
+//Car highestHorsepower = CarsData.Cars
+//                        .OrderByDescending(x => x.HorsePower)
+//                        .First();
+//Console.WriteLine(highestHorsepower.Model);
+
+//// 6 baranje
+//List<Car> allChevroletCars = CarsData.Cars
+//                        .OrderByDescending(x => x.Weight)
+//                        .Where(x => x.Model.StartsWith("Chevrolet"))
+//                        .ToList();
+//allChevroletCars.PrintEntities();
+
+//// 7 baranje
+//Car longestName = CarsData.Cars
+//            .OrderByDescending(x => x.Model.Length)
+//            .First();
+//Console.WriteLine(longestName.Model);
+
+////8 baranje
+
+
+////9 baranje
+//List<Car> top5Hp = CarsData.Cars
+//              .OrderByDescending(x => x.HorsePower)
+//              .Take(5)
+//              .ToList();
+//top5Hp.PrintEntities();
+
+////10 baranje
+//Car highestAcc = CarsData.Cars
+//               .OrderByDescending(x => x.AccelerationTime)
+//               .First();
+//Console.WriteLine(highestAcc.Model);
+
+////11 baranje
+//List<string> greaterThan200Hp = CarsData.Cars
+//            .Where(x => x.HorsePower > 200)
+//            .Select(x => $"{x.Model} {x.HorsePower} hp")
+//            .ToList();
+
+//greaterThan200Hp.PrintSimple();
+
+////12 baranje
+//List<string> uniqueOriginsOfCars = CarsData.Cars
+//             .Select(x => x.Origin)
+//             .Distinct()
+//             .ToList();
+//uniqueOriginsOfCars.PrintSimple();
+
+////13 baranje
+//List<string> moreThan4Cilinders = CarsData.Cars
+//            .Where(x => x.Cylinders >= 4)
+//            .OrderBy(x => x.Origin)
+//            .OrderBy(x => x.HorsePower)
+//            .Select(x => x.Model)
+//            .ToList();
+//moreThan4Cilinders.PrintSimple();
+
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+
+////14 baranje
+//List<Car> moreThan6Cilinders = CarsData.Cars
+//            .Where(x => x.Cylinders > 6)
+//            .Concat(
+//            CarsData.Cars.Where(y => y.Cylinders == 4 && y.HorsePower > 110.0))
+//            .ToList();
+//moreThan6Cilinders.PrintEntities();
+
+////15 baranje
+//List<Car> moreThan200Hp = CarsData.Cars
+//            .Where(x => x.HorsePower > 200)
+//            .ToList();
+
+//double minimum = moreThan200Hp.Min(x => x.MilesPerGalon) ;
+//double maximum = moreThan200Hp.Max(x => x.MilesPerGalon);
+//double average = moreThan200Hp.Average(x => x.MilesPerGalon);
+
+//Console.WriteLine($"The minimum miles per galon car is {minimum}");
+//Console.WriteLine($"The maximum miles per galon car is {maximum}");
+//Console.WriteLine($"The average miles per galon car is {average}");
+
+////16 baranje
+//List<string> smh = CarsData.Cars
+//        .OrderByDescending(x => x.Model)
+//        .Where(x => x.Model.StartsWith("M") && x.HorsePower > 120)
+//        .Select(x => x.Model)
+//        .ToList();
+//smh.PrintSimple();
+
+////17 baranje
+//List<Car> smh2 = CarsData.Cars
+//        .Where(x => x.Cylinders > 4 && x.HorsePower > 200)
+//        .OrderBy(x => x.AccelerationTime)
+//        .Take(5)
+//        .ToList();
+//smh2.PrintEntities();
+//Console.ReadLine();
+
+// 1 baranje
+List<Car> allCarsFromEurope = CarsData.Cars
+                        .Where(x => x.Origin == "Europe").ToList();
+Console.WriteLine("All cars from Europe");
+allCarsFromEurope.PrintEntities();
+
+//2 baranje
+List<Car> moreThan6Cilinders = CarsData.Cars
+            .Where(x => x.Cylinders > 6)
+            .Concat(
+            CarsData.Cars.Where(y => y.Cylinders == 4 && y.HorsePower > 110.0))
+            .ToList();
+Console.WriteLine("More than 6 cylinders");
+moreThan6Cilinders.PrintEntities();
+
+//3 baranje
+
+Console.WriteLine("Cars by origin : ");
+var carCountByOrigin = CarsData.Cars
+    .GroupBy(x => x.Origin)
+    .Select(x => (Origin: x.Key, Count: x.Count()));
+
+foreach (var originGroup in carCountByOrigin)
+{
+    Console.WriteLine($"{originGroup.Origin} {originGroup.Count} models");
+}
+
+
+//4 baranje
+Console.WriteLine("Max min and avg mpg");
+List<Car> moreThan200Hp = CarsData.Cars
+            .Where(x => x.HorsePower > 200)
+            .ToList();
+
+double minimum = moreThan200Hp.Min(x => x.MilesPerGalon );
+double maximum = moreThan200Hp.Max(x => x.MilesPerGalon);
+double average = moreThan200Hp.Average(x => x.MilesPerGalon);
+
+Console.WriteLine($"The minimum miles per galon on a car is {minimum} mpg");
+Console.WriteLine($"The maximum miles per galon on a car is {maximum} mpg");
+Console.WriteLine($"The average miles per galon on a car is {average} mpg");
+
+
+//5 baranje
+Console.WriteLine("Fastest 3 accelerating cars");
+
+List < Car > fastestAccCars = CarsData.Cars
+            .OrderBy(x => x.AccelerationTime)
+            .Take(3)
+            .ToList();
+
+fastestAccCars.PrintEntities();
+
+foreach(Car fastestCar in fastestAccCars)
+{
+    Console.WriteLine(fastestCar.Model);
+}
+
+
+//6 baranje
+
+double totalWeight = CarsData.Cars
+            .Where(x => x.Cylinders > 6)
+            .Sum(x => x.Weight);
+Console.WriteLine($"the total weight of cars with more than 6 cylinders is {totalWeight}");
+
+
+
+//7 baranje e istoto kako 6
+
+//8 baranje
+//Find the average MilesPerGalon for cars with even number of cylinders:
+double avgMilesEvenCyl = CarsData.Cars
+                .Where(x => x.Cylinders % 2 == 0)
+                .Average(x => x.MilesPerGalon);
+Console.WriteLine($"The average mpg for cars with even number of cylinders is {avgMilesEvenCyl}");       
+
+Console.ReadLine();
